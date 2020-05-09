d911e44b1fa6e9118226d3a0dc778c7d236276d9
William Goh <wirriamm@gmail.com>
2020-05-09 13:30:58 +0800

    generates answer based on question in answer method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,12 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
+    if @question.downcase == 'i am going to work'
+      @answer = 'Great!'
+    elsif @question[-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
   end
 end
